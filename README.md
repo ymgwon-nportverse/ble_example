ed5fd965d9eee12b3a99533c1387263acf45e16f
ymgwon-nportverse <ymgwon@nportverse.com>
2023-12-14 12:06:46 +0900

    🎉 init: 프로젝트 초기 설정

---
+++ README.md
@@ -0,0 +1,3 @@
+# ble_test
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/nportverse/ble_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nportverse.ble_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
