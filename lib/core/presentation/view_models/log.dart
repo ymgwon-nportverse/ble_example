bb84a4784b94ab0d8dbc98bc50d5db0c2d80b593
leeahhyun <ahhyun@nportverse.com>
2023-12-20 14:44:10 +0900

    fix: 로그 클래스 디테일하게 변경

--- lib/core/presentation/view_models/log.dart
+++ lib/core/presentation/view_models/log.dart
@@ -1,23 +1,33 @@
 import 'dart:typed_data';
 
+import 'package:bluetooth_low_energy/bluetooth_low_energy.dart';
 import 'package:convert/convert.dart';
 import 'package:intl/intl.dart';
 
 class Log {
-  Log(
-    this.type,
-    this.value, [
+  Log({
+    required this.type,
+    required this.value,
     this.detail,
-  ]) : time = DateTime.now();
-
+    this.offset,
+    this.id,
+    this.centralUuid,
+    this.characteristicUuid,
+    this.state,
+  }) : time = DateTime.now();
   final DateTime time;
   final LogType type;
   final Uint8List value;
   final String? detail;
+  final int? offset;
+  final int? id;
+  final UUID? centralUuid;
+  final UUID? characteristicUuid;
+  final bool? state;
 
   @override
   String toString() {
-    final type = this.type.name;
+    final type = this.type.toString().split('.').last;
     final formatter = DateFormat.Hms();
     final time = formatter.format(this.time);
     final message = hex.encode(value);
